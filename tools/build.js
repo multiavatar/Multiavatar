faa9a22aebb76bd992a0296a1528c31a6f0237ec
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-15 19:04:17 -0800

    build: umd build

--- tools/build.js
+++ tools/build.js
@@ -4,5 +4,21 @@ const originalFile = fs.readFileSync(path.join(__dirname, "../multiavatar.min.js
 
 if (!fs.existsSync(path.join(__dirname, "../dist"))) fs.mkdirSync(path.join(__dirname, "../dist"))
 
+const umd = (content) => `
+    (function (global, factory) {
+        if (typeof define === "function" && define.amd) {
+            define(factory());
+        } else if (typeof module === "object" && module.exports) {
+            module.exports = factory();
+        } else {
+            global.multiavatar = factory();
+        }
+    })(typeof self !== "undefined" ? self : this, function () {
+        ${content};
+        return multiavatar;
+    })
+`
+
 fs.writeFileSync(path.join(__dirname, "../dist/multiavatar.es.js"), `${originalFile.toString()};export default multiavatar;`)
 fs.writeFileSync(path.join(__dirname, "../dist/multiavatar.cjs"), `${originalFile};module.exports=multiavatar;`);
+fs.writeFileSync(path.join(__dirname, "../dist/multiavatar.umd.js"), umd(originalFile));
