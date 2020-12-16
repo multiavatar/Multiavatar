558a1c778e6234ce373f92d5555949f7aeb7025e
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-12-16 08:02:11 -0800

    add comment

--- tools/build.js
+++ tools/build.js
@@ -4,6 +4,8 @@ const originalFile = fs.readFileSync(path.join(__dirname, "../multiavatar.min.js
 
 if (!fs.existsSync(path.join(__dirname, "../dist"))) fs.mkdirSync(path.join(__dirname, "../dist"))
 
+//TODO - use terser to minify builds
+
 const umd = (content) => `
     (function (global, factory) {
         if (typeof define === "function" && define.amd) {
